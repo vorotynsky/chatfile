f483ce3210a7b549a400a959c5383db5ddc879d3
Maxim Vorotynsky <vorotynsky.maxim@gmail.com>
2025-05-17 15:39:01 +0000

    test: collect chat context

---
+++ app/context_test.go
@@ -0,0 +1,40 @@
+package main
+
+import (
+	"bufio"
+	"chatfile/test"
+	"fmt"
+	"io"
+	"strings"
+	"testing"
+)
+
+type TestHistory struct {
+	w io.Writer
+	c *Context
+}
+
+func (h *TestHistory) Append(role Role, message string) {
+	if strings.IndexByte(message, '\n') > 0 {
+		_, _ = fmt.Fprintf(h.w, "[%s] %s:\n%s\n", h.c.CurrentModel, role, message)
+	} else {
+		_, _ = fmt.Fprintf(h.w, "[%s] %s: %s\n", h.c.CurrentModel, role, message)
+	}
+}
+
+func TestContextCollecting(t *testing.T) {
+	test.DoTest(t, "collected", func(t *testing.T, input io.Reader, output io.Writer) {
+		history := &TestHistory{w: output}
+		context := &Context{History: history}
+		history.c = context
+
+		reader := bufio.NewReader(input)
+		lexer := NewLexer(reader)
+		scanner := NewParseScanner(lexer)
+
+		for scanner.Scan() {
+			command := scanner.Command()
+			command.Apply(context)
+		}
+	})
+}
