1f5772d1884bee4de79e344ce14476a1d93d171c
Maxim Vorotynsky <vorotynsky.maxim@gmail.com>
2025-05-26 23:01:35 +0200

    configure `seed` and `temperature` using cli arg

--- lib/llm.go
+++ lib/llm.go
@@ -6,6 +6,7 @@ import (
 	"io"
 
 	"github.com/openai/openai-go"
+	"github.com/openai/openai-go/packages/param"
 )
 
 type OpenAiHistory struct {
@@ -23,11 +24,29 @@ func (h *OpenAiHistory) Append(role Role, message string) {
 	}
 }
 
+type RequestParams struct {
+	Seed        param.Opt[int64]
+	Temperature param.Opt[float64]
+}
+
+func NewParameters(seed *int64, temperature *float64) RequestParams {
+	return RequestParams{toOpt(seed), toOpt(temperature)}
+}
+
+func toOpt[T comparable](temperature *T) param.Opt[T] {
+	if temperature != nil {
+		return param.NewOpt(*temperature)
+	}
+	return param.NullOpt[T]()
+}
+
 // Send a streaming request and write the response content to the provided writer in chunks as they arrive.
-func Send(client openai.Client, model ModelName, history OpenAiHistory, writer io.StringWriter) (err error) {
+func Send(client openai.Client, model ModelName, history OpenAiHistory, writer io.StringWriter, params RequestParams) (err error) {
 	stream := client.Chat.Completions.NewStreaming(context.Background(), openai.ChatCompletionNewParams{
-		Model:    string(model),
-		Messages: history.messages,
+		Model:       string(model),
+		Messages:    history.messages,
+		Temperature: params.Temperature,
+		Seed:        params.Seed,
 	})
 
 	defer stream.Close()
